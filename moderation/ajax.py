854f7f208a5439da9ff21cda06ed341477c9f900
Brad Choate <brad@bradchoate.com>
2009-09-09 17:45:55 -0700

    Adjusted columns-- added userpic. Support for multiple item operations.

--- moderation/ajax.py
+++ moderation/ajax.py
@@ -9,6 +9,7 @@ import simplejson as json
 
 import typepad
 from typepadapp.decorators import ajax_required
+from typepadapp.models import OAuthClient, User
 from moderation.models import Asset, Flag, AssetContent
 
 from oauth import oauth
@@ -21,62 +22,143 @@ def moderate(request):
     """
     res = 'OK'
 
-    asset_id = request.POST.get('asset_id')
-    if not asset_id:
+    asset_ids = request.POST.getlist('asset_id')
+
+    if asset_ids is None:
         raise http.Http404
 
-    asset = Asset.objects.get(id=asset_id)
-    if not asset:
-        raise http.Http404
+    action = request.POST.get('action', None)
 
-    action = request.POST.get('action', 'approve')
-    if action == 'approve':
-        if asset.status in [Asset.FLAGGED, Asset.SUPPRESSED]:
-            # clear any flags too while we're at it
-            Flag.objects.filter(asset=asset).delete()
-            # leaving the asset as approved prevents it from being
-            # flagged again
-            asset.flag_count = 0
-            asset.status = Asset.APPROVED
-            asset.save()
+    success = []
+    fail = []
+    ban_list = []
+    for asset_id in asset_ids:
+        try:
+            asset = Asset.objects.get(id=asset_id)
+        except:
+            fail.append(asset_id)
+            continue
 
-        elif asset.status == Asset.MODERATED:
-            content = AssetContent.objects.get(asset=asset)
+        if action == 'approve':
+            if asset.status in (Asset.FLAGGED, Asset.SUPPRESSED):
+                # clear any flags too while we're at it
+                Flag.objects.filter(asset=asset).delete()
+                # leaving the asset as approved prevents it from being
+                # flagged again
+                asset.flag_count = 0
+                asset.status = Asset.APPROVED
+                asset.save()
 
-            tp_asset = typepad.Asset.from_dict(json.loads(content.data))
-            # FIXME: https://intranet.sixapart.com/bugs/default.asp?88475
-            # if content.attachment:
-            #     tp_asset.file = content.attachment
+            elif asset.status == Asset.MODERATED:
+                content = asset.content
+                tp_asset = typepad.Asset.from_dict(json.loads(content.data))
 
-            # setup credentials of post author
-            backend = urlparse(settings.BACKEND_URL)
-            typepad.client.clear_credentials()
-            token = oauth.OAuthToken.from_string(content.user_token)
-            typepad.client.add_credentials(request.oauth_client.consumer,
-                token, domain=backend[1])
+                # setup credentials of post author
+                backend = urlparse(settings.BACKEND_URL)
+                typepad.client.clear_credentials()
+                token = oauth.OAuthToken.from_string(content.user_token)
+                typepad.client.add_credentials(request.oauth_client.consumer,
+                    token, domain=backend[1])
 
-            # TODO: check for fail
-            if tp_asset.type_id == 'comment':
-                typepad.client.batch_request()
-                post = typepad.Asset.get_by_url_id(tp_asset.in_reply_to.url_id)
-                typepad.client.complete_batch()
-                post.comments.post(tp_asset)
-            else:
-                tp_asset.save(group=request.group)
+                # TODO: check for fail
+                if tp_asset.type_id == 'comment':
+                    typepad.client.batch_request()
+                    post = typepad.Asset.get_by_url_id(tp_asset.in_reply_to.url_id)
+                    typepad.client.complete_batch()
+                    post.comments.post(tp_asset)
+                else:
+                    if content.attachment.name:
+                        # file based asset; upload using signed browser upload
+                        oauth_client = OAuthClient(request.application)
+                        oauth_client.token = token
+                        remote_url = request.application.browser_upload_endpoint
+                        endpoint = oauth_client.get_file_upload_url(remote_url)
+                        tp_asset.save(endpoint, group=request.group, file=content.attachment)
+                    else:
+                        tp_asset.save(group=request.group)
+
+                asset.delete()
+
+            success.append(asset_id)
+
+        elif action in ('delete', 'ban'):
+            # outright delete it?? or do we have a status for this?
+            if asset.asset_id:
+                if action == 'ban':
+                    if asset.user_id not in ban_list:
+                        # also ban this user
+                        typepad.client.batch_request()
+                        user_memberships = User.get_by_url_id(asset.user_id).memberships.filter(by_group=request.group)
+                        typepad.client.complete_batch()
+
+                        try:
+                            user_membership = user_memberships[0]
+
+                            if user_membership.is_admin():
+                                # cannot ban/unban another admin
+                                fail.append(asset_id)
+                                continue
+
+                            user_membership.block()
+                            ban_list.append(asset.user_id)
+
+                        except IndexError:
+                            pass # no membership exists; ignore ban
+
+                # we need to remove from typepad
+                tp_asset = typepad.Asset.get_by_url_id(asset.asset_id)
+                tp_asset.delete()
 
             asset.delete()
-        res = 'Post approved.'
+            success.append(asset_id)
 
-    elif action == 'delete':
-        # outright delete it?? or do we have a status for this?
-        if asset.asset_id:
-            # we need to remove from typepad
-            tp_asset = typepad.Asset.get_by_url_id(asset.asset_id)
-            tp_asset.delete()
+        elif action == 'view':
+            if asset.status in (Asset.MODERATED, Asset.SPAM):
+                content = AssetContent.objects.get(asset=asset)
+                data = json.loads(content.data)
+                # supplement with a fake author member, since this isn't
+                # populated into the POSTed data for pre-moderation/spam assets
+                data['author'] = {
+                    'displayName': asset.user_display_name,
+                    'links': [{
+                        'rel': 'avatar',
+                        'href': asset.user_userpic,
+                        'width': 50,
+                        'height': 50
+                    }]
+                }
+                tp_asset = typepad.Asset.from_dict(data)
+                tp_asset.published = asset.ts
+            else:
+                typepad.client.batch_request()
+                tp_asset = typepad.Asset.get_by_url_id(asset.asset_id)
+                typepad.client.complete_batch()
 
-        asset.delete()
-        res = 'Post deleted.'
-    else:
-        return http.HttpResponseForbidden("invalid request")
+            event = typepad.Event()
+            event.object = tp_asset
+            event.actor = tp_asset.author
+            event.published = tp_asset.published
 
-    return http.HttpResponse(res)
+            # 'view' of 'permalink' causes the full content to render
+            data = { 'entry': tp_asset, 'event': event, 'view': 'permalink' }
+            res = render_to_string("motion/assets/asset.html", data,
+                context_instance=RequestContext(request))
+
+            return http.HttpResponse(res)
+
+        else:
+            return http.HttpResponseForbidden('{"message":"invalid request"}')
+
+    if action == 'delete':
+        res = 'You deleted %d posts.' % len(success)
+    elif action == 'approve':
+        res = 'You approved %d posts.' % len(success)
+    elif action == 'ban':
+        res = 'You banned %d users.' % len(ban_list)
+
+    data = json.dumps({
+        "success": success,
+        "fail": fail,
+        "message": res
+    })
+    return http.HttpResponse(data)
--- moderation/static/js/moderation.js
+++ moderation/static/js/moderation.js
@@ -4,51 +4,125 @@ var settings = {
 
 // Moderate action
 function moderate(id, action) {
-    var asset_id = id.split('-')[1]
-    $("#loader-" + asset_id).toggle()
-    $("#item-" + asset_id).toggle()
+    var asset_id = id.split('-')[1];
+    var asset_ids = [];
+    if (asset_id == 'checked') {
+        $(".item .cb:checked").each(function() {
+            asset_id = $(this).val();
+            $("#loader-" + asset_id).show();
+            $("#item-" + asset_id).hide();
+            asset_ids.push(asset_id);
+        });
+    }
+    else {
+        $("#loader-" + asset_id).show();
+        $("#item-" + asset_id).hide();
+        asset_ids = [ asset_id ];
+    }
     $.ajax({
         type: "POST",
         url: settings.moderate_url,
-        data: {"asset_id": asset_id, "action": action},
-        success: function(data){
-            $("#loader-" + asset_id).toggle()
-            // decrement asset count
+        data: { "asset_id": asset_ids, "action": action },
+        dataType: "json",
+        success: function(data) {
+            // hide progress display for rows we've processed
+            for (var id in data['success']) {
+                $("#loader-" + data['success'][id]).hide();
+            }
+
+            // redisplay assets that failed to approve/delete/etc.
+            for (var id in data['fail']) {
+                $("#loader-" + data['fail'][id]).hide();
+                $("#item-" + data['fail'][id]).show();
+            }
+
+            // adjust asset count
             var count = parseInt($('#asset-count').html());
             if (isNaN(count))
                 count = 0;
             else
-                count--;
-            $('#asset-count').html(''+count)
+                count -= data['success'].length;
+
+            $('#asset-count').html(''+count);
             // show message if no assets left
             if (!count) {
-                $('#assets-empty').toggle()
-                $('#assets-footer').toggle()
+                $('#assets-empty').show();
+                $('#assets-footer').show();
             }
+
             // show flash
-            $('#flash-notice').html(''+data)
-            $('#flash-notice').toggle()
+            $('#flash-notice').html(data['message']);
+            $('#flash-notice').show();
         },
         error: function(xhr, txtStatus, errorThrown) {
             alert('An error occurred: ' + xhr.status + ' -- ' + xhr.statusText);
-            $("#loader-" + asset_id).toggle()
-            $("#item-" + asset_id).toggle()
+            for (var id in asset_ids) {
+                $("#loader-" + asset_ids[id]).hide();
+                $("#item-" + asset_ids[id]).show();
+            }
+        }
+    });
+}
+
+function view(id) {
+    var asset_id = id.split('-')[1]
+    $.ajax({
+        type: "POST",
+        url: settings.moderate_url,
+        data: {"asset_id": asset_id, "action": "view"},
+        success: function(data){
+            // decrement asset count
+            $('#view-detail').html(data);
+            $('#view-dialog').dialog('open');
+        },
+        error: function(xhr, txtStatus, errorThrown) {
+            alert('An error occurred: ' + xhr.status + ' -- ' + xhr.statusText);
         }
     });
 }
 
 $(document).ready(function () {
     $('.approve').click(function() {
-        moderate($(this).attr('id'), 'approve');
-        return false;
-    });
-    $('.delete').click(function() {
-        if (confirm('Are you sure you want to delete this post?')) {
-            moderate($(this).attr('id'), 'delete');
+        id = $(this).attr('id');
+        if (id.indexOf('-checked') >= 0) {
+            if (!confirm('Approve the selected items?'))
+                return false;
         }
+        moderate(id, 'approve');
         return false;
     });
+
+    $('.delete').click(function() {
+        id = $(this).attr('id');
+        if (id.indexOf('-checked') >= 0) {
+            if (!confirm('Delete the selected items?'))
+                return false;
+        } else if (!confirm('Are you sure you want to delete this post?')) {
+            return false;
+        }
+        moderate($(this).attr('id'), 'delete');
+        return false;
+    });
+
+    $('.ban').click(function() {
+        id = $(this).attr('id');
+        if (id.indexOf('-checked') >= 0) {
+            if (!confirm('Ban users who posted these items (also deletes selected posts)?'))
+                return false;
+        } else if (!confirm('Are you sure you want to ban this user (also deletes this post)?')) {
+            return false;
+        }
+        moderate(id, 'ban');
+        return false;
+    });
+
+    $('.view').click(function() {
+        if (!$(this).attr('id')) return true;
+        view($(this).attr('id'), 'view');
+        return false;
+    });
+
     $('.check-all').click(function() {
-        $('[type="checkbox"]').attr('checked', $(this).attr('checked') ? 'checked': false);
+        $('.item .cb[type="checkbox"]').attr('checked', $(this).attr('checked') ? 'checked': false);
     });
 })
