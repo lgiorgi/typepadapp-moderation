3b1c2306fee9e71353ebad2bc608e7c9411bccb0
Brad Choate <brad@bradchoate.com>
2009-11-16 16:34:36 -0800

    Updating for gh-pages publishing.

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.abspath('exts'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'github.tools.sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
