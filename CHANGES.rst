bca26f00016b4ce499efbf294f9224fa99dd8e9e
Brad Choate <brad@bradchoate.com>
2010-02-05 15:27:39 -0800

    Fixed request assertions for ajax method.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,24 @@
 typepadapp-moderation Changelog
 ===============================
 
+Unreleased
+----------
+
+* Added views for listing users with a pre-moderation, blocked status.
+* Now displaying a reason for why a user has been given a pre-moderation or blocked status.
+
+
+1.1a1 (2010-1-20)
+-----------------
+
+* Added keyword moderation support.
+* Added new user probation feature.
+* Added post throttle support.
+* Fixed a bug affecting banning users within the moderation admin panel.
+* Fixed some IE layout issues for the moderation dialog.
+* Changed user notification for moderated comments.
+
+
 1.0.2 (2009-12-16)
 ------------------
 
--- moderation/ajax.py
+++ moderation/ajax.py
@@ -49,6 +49,16 @@ def moderate(request):
     """
     Moderation actions for the moderation queue. Approve or delete. Return OK.
     """
+
+    if not hasattr(request, 'typepad_user'):
+        typepad.client.batch_request()
+        request.typepad_user = get_user(request)
+        typepad.client.complete_batch()
+
+    if not (request.typepad_user.is_authenticated() and \
+        request.typepad_user.is_superuser):
+        return http.HttpResponseForbidden()
+
     res = 'OK'
 
     item_ids = request.POST.getlist('item_id')
@@ -212,7 +222,8 @@ def moderate(request):
             success.append(item_id)
 
         else:
-            return http.HttpResponseForbidden('{"message":"invalid request"}')
+            return http.HttpResponseForbidden('{"message":"invalid request"}',
+                mimetype='application/json')
 
     if action == 'delete':
         res = 'You deleted %d post(s).' % len(success)
@@ -228,7 +239,7 @@ def moderate(request):
         "fail": fail,
         "message": res
     })
-    return http.HttpResponse(data)
+    return http.HttpResponse(data, mimetype='application/json')
 
 
 signals.asset_deleted.connect(models.clear_local_data_for_asset)
